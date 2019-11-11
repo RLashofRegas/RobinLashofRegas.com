8bb62f7cc20124d1df4c0341b7b785cab4a13252
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2019-11-11 13:38:05 -0800

    fix trailing whitespace

--- src/webspa/ClientApp/src/app/app-routing.module.ts
+++ src/webspa/ClientApp/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { 
+  {
     path: 'blog',
     loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
   },
-  { 
+  {
     path: '**',
     redirectTo: 'blog'
   }
