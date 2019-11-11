5e5ab3fd3ea7ea550bfdd1b0c69f35be192d5663
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2019-11-11 15:04:33 -0800

    move options service add IAppOptions

--- src/webspa/ClientApp/src/app/shared/services/options.service.ts
+++ src/webspa/ClientApp/src/app/shared/services/options.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
