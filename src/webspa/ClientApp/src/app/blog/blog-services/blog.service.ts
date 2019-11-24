a91a9d616ac8345f76d2987c4ebe4bdb65656b27
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2019-11-24 10:06:53 -0800

    fix tslint errors

--- src/webspa/ClientApp/src/app/blog/blog-services/blog.service.ts
+++ src/webspa/ClientApp/src/app/blog/blog-services/blog.service.ts
@@ -2,33 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, Subject } from 'rxjs';
-import { shareReplay } from "rxjs/operators";
+import { shareReplay } from 'rxjs/operators';
 
-import { BlogServicesModule } from "./blog-services.module";
+import { BlogServicesModule } from './blog-services.module';
 import { IAppOptions } from 'src/app/shared/models/app-options.model';
 import { OptionsService } from 'src/app/shared/services/options.service';
-import { IBlog } from "../models/blog.model";
+import { IBlog } from '../models/blog.model';
 
 @Injectable({
   providedIn: BlogServicesModule
 })
 export class BlogService {
-  private appOptions: Observable<IAppOptions>
+  private appOptions: Observable<IAppOptions>;
 
-  constructor(private optionsService: OptionsService, private http: HttpClient) { 
-    this.appOptions = optionsService.options().pipe(shareReplay(1))
+  constructor(private optionsService: OptionsService, private http: HttpClient) {
+    this.appOptions = optionsService.options().pipe(shareReplay(1));
   }
 
   getBlogs(): Observable<IBlog[]> {
-    let blogsSubject = new Subject<IBlog[]>();
+    const blogsSubject = new Subject<IBlog[]>();
     this.appOptions.subscribe(
       (options) => {
-        let url = options.blogAPIUrl + "/Blogs";
+        const url = options.blogAPIUrl + '/Blogs';
         this.http
           .get<IBlog[]>(url)
           .subscribe(
             (blogs) => {
-              blogsSubject.next(blogs)
+              blogsSubject.next(blogs);
             }
           );
       }
