893b35aa367b1b7dfc5b2cd40426602c49dbf4d1
Robin Lashof-Regas <Robin.Lashof@gmail.com>
2019-12-19 22:17:53 -0800

    fix linter errors

--- src/webspa/ClientApp/src/app/blog/topic-tiles/topic-tile/topic-tile.component.ts
+++ src/webspa/ClientApp/src/app/blog/topic-tiles/topic-tile/topic-tile.component.ts
@@ -8,12 +8,12 @@ import {
   transition,
   animateChild,
   query
-} from '@angular/animations'
+} from '@angular/animations';
 
-const hoveredState: string = 'hovered';
-const notHoveredState: string = 'notHovered';
-const fadeInTime: number = 200; // in ms
-const fadeOutTime: number = 100; // in ms
+const hoveredState = 'hovered';
+const notHoveredState = 'notHovered';
+const fadeInTime = 200; // in ms
+const fadeOutTime = 100; // in ms
 
 @Component({
   selector: 'app-topic-tile',
@@ -43,10 +43,10 @@ const fadeOutTime: number = 100; // in ms
     ]),
     trigger('titleAnimation', [
       state(notHoveredState, style({
-        "font-size": "50px"
+        'font-size': '50px'
       })),
       state(hoveredState, style({
-        "font-size": "60px"
+        'font-size': '60px'
       })),
       transition(notHoveredState + ' => ' + hoveredState, [
         animate(fadeInTime)
