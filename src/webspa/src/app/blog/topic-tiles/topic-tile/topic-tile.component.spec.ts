e8cc890e2215664d3a8d1d3114fcf938b43b1f54
Robin Lashof-Regas <Robin.Lashof-Regas@Itron.com>
2022-06-05 20:28:33 +0000

    upgrade angular

--- src/webspa/src/app/blog/topic-tiles/topic-tile/topic-tile.component.spec.ts
+++ src/webspa/src/app/blog/topic-tiles/topic-tile/topic-tile.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TopicTileComponent } from './topic-tile.component';
 
@@ -6,7 +6,7 @@ describe('TopicTileComponent', () => {
   let component: TopicTileComponent;
   let fixture: ComponentFixture<TopicTileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     void TestBed.configureTestingModule({
       declarations: [ TopicTileComponent ]
     })
